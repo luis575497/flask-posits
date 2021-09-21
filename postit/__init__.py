e033a4966d91b1e42fe0fd1c48f28677c25e0edc
Luis Enrique Lescano Borrego <luis575497@gmail.com>
2021-09-20 19:10:32 -0500

    Commando de crear tablas de base de datos ejecutando flask ini-db añadido

--- postit/__init__.py
+++ postit/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import click
+from flask.cli import with_appcontext
 
 db = SQLAlchemy()
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Create Database Tables"""
+    db.drop_all()
+    db.create_all()
+    click.echo("Base de datos inicializada")
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
+
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config = True)
@@ -9,9 +24,11 @@ def create_app(test_config=None):
 
     # Iniciar configuracion de base de datos
     db.init_app(app)
-
+    init_app(app)
     with app.app_context():
         from . import routes
-        db.create_all()
 
         return app
+
+
+
