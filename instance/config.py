91218654b0d13e1aa2e9a77f93262ebe07bc42c9
Luis Enrique Lescano Borrego <luis575497@gmail.com>
2021-09-22 13:19:42 -0500

    Arreglado la configuracion de track de sqlalchemy

--- instance/config.py
+++ instance/config.py
@@ -7,8 +7,12 @@ load_dotenv(path.join(basedir,'.env'))
 
 TESTING = True
 DEBUG = True
+
+FLASK_APP = 'postit'
 FLASK_ENV = 'development'
 SECRET_KEY = environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = "postgresql://posit:posit@localhost:5432/posit"
-SQLALCHEMY_TRACK_MODIFICATIOS = False
 
+# Flask SqlAlchemy
+SQLALCHEMY_DATABASE_URI = "postgresql://posit:posit@localhost:5432/posit"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
